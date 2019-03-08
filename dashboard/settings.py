e22a4588a8c3f53da1a67b8617385ce605586a2f
atyoshimatsu <46603382+atyoshimatsu@users.noreply.github.com>
2019-03-08 19:27:58 +0900

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -25,6 +25,22 @@ SECRET_KEY = '<secret_key>'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 ALLOWED_HOSTS = ['titanic-dashboard.herokuapp.com']
 
 
